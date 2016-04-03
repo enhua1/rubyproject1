6d6c9b53281b5804d9bfbcaa1e8f3356d5095ad7
enhua <enhuathegreat@gmail.com>
2016-04-03 22:17:01 +0800

    partials and wrap controller section

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all #plural
@@ -28,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path
@@ -39,19 +40,17 @@ class ArticlesController < ApplicationController
 
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
     
 
   def show
-  
-    @article = Article.find(params[:id])
     
   end
 
 
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -59,7 +58,11 @@ class ArticlesController < ApplicationController
 
 
   private
-    
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
